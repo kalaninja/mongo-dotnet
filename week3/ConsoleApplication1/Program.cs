ad9ace3b068d787d0138ab2e096028fc8265f578
Alexander Kalankhodzhaev <kalansoft@gmail.com>
2019-08-11 20:15:54 +0300

    mongo-dotnet

---
+++ week3/ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApplication1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MainAsync().Wait();
+
+			Console.WriteLine("Press any key");
+			Console.ReadLine();
+		}
+
+		private async static Task MainAsync()
+		{
+			var client = new MongoClient();
+			var db = client.GetDatabase("school");
+
+			var studentsCollection = db.GetCollection<Student>("students");
+			var students = await studentsCollection.Find(new BsonDocument()).ToListAsync();
+
+			foreach (var student in students)
+			{
+				var score = student.Scores.Where(x => x.Type.ToLower() == "homework").OrderBy(x => x.ScoreValue).First();
+				student.Scores.Remove(score);
+
+				studentsCollection.ReplaceOne(x => x.Id == student.Id, student);
+			}
+		}
+
+		private class Student
+		{
+			public int Id { get; set; }
+
+			[BsonElement("name")]
+			public string Name { get; set; }
+
+			[BsonElement("scores")]
+			public IList<Score> Scores { get; set; }
+		}
+
+		private class Score
+		{
+			[BsonElement("type")]
+			public string Type { get; set; }
+
+			[BsonElement("score")]
+			public double ScoreValue { get; set; }
+		}
+	}
+}
---
+++ week3/homework_3_2_making_your_blog_accept_posts/homework_3.2/3.2/src/M101DotNet.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using M101DotNet.WebApp.Models;
+using M101DotNet.WebApp.Models.Home;
+using MongoDB.Bson;
+using System.Linq.Expressions;
+
+namespace M101DotNet.WebApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		public async Task<ActionResult> Index()
+		{
+			var blogContext = new BlogContext();
+
+			// XXX WORK HERE
+			// find the most recent 10 posts and order them
+			// from newest to oldest
+			var recentPosts = await blogContext.Posts.Find(new BsonDocument())
+				.SortByDescending(x => x.CreatedAtUtc)
+				.ToListAsync();
+
+			var model = new IndexModel
+			{
+				RecentPosts = recentPosts
+			};
+
+			return View(model);
+		}
+
+		[HttpGet]
+		public ActionResult NewPost()
+		{
+			return View(new NewPostModel());
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> NewPost(NewPostModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
+
+			var blogContext = new BlogContext();
+
+			// XXX WORK HERE
+			// Insert the post into the posts collection
+			var post = new Post
+			{
+				Author = this.User.Identity.Name,
+				Title = model.Title,
+				Content = model.Content,
+				Tags = model.Tags.Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries).ToList(),
+				CreatedAtUtc = DateTime.Now,
+				Comments = new List<Comment>()
+			};
+
+			await blogContext.Posts.InsertOneAsync(post);
+
+			return RedirectToAction("Post", new { id = post.Id });
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> Post(string id)
+		{
+			var blogContext = new BlogContext();
+
+			// XXX WORK HERE
+			// Find the post with the given identifier
+			var post =
+				await blogContext.Posts.Find(Builders<Post>.Filter.Eq(x => x.Id, ObjectId.Parse(id))).FirstOrDefaultAsync();
+
+			if (post == null)
+			{
+				return RedirectToAction("Index");
+			}
+
+			var model = new PostModel
+			{
+				Post = post
+			};
+
+			return View(model);
+		}
+
+		[HttpGet]
+		public async Task<ActionResult> Posts(string tag = null)
+		{
+			var blogContext = new BlogContext();
+
+			// XXX WORK HERE
+			// Find all the posts with the given tag if it exists.
+			// Otherwise, return all the posts.
+			// Each of these results should be in descending order.
+			var filter = tag != null ? new BsonDocument("Tags", tag) : new BsonDocument();
+			var posts = await blogContext.Posts.Find(filter).SortByDescending(x => x.CreatedAtUtc).ToListAsync();
+
+			return View(posts);
+		}
+
+		[HttpPost]
+		public async Task<ActionResult> NewComment(NewCommentModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return RedirectToAction("Post", new { id = model.PostId });
+			}
+
+			var blogContext = new BlogContext();
+			// XXX WORK HERE
+			// add a comment to the post identified by model.PostId.
+			// you can get the author from "this.User.Identity.Name"
+			var post =
+				await
+					blogContext.Posts.Find(Builders<Post>.Filter.Eq(x => x.Id, ObjectId.Parse(model.PostId))).FirstOrDefaultAsync();
+
+			if (post != null)
+			{
+				post.Comments.Add(new Comment
+				{
+					Author = this.User.Identity.Name,
+					Content = model.Content,
+					CreatedAtUtc = DateTime.Now
+				});
+
+				await blogContext.Posts.ReplaceOneAsync(Builders<Post>.Filter.Eq(x => x.Id, ObjectId.Parse(model.PostId)), post);
+			}
+
+			return RedirectToAction("Post", new { id = model.PostId });
+		}
+	}
+}
